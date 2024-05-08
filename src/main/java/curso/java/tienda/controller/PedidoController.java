7132ee485940117361cfb1e1d59716beafdcd675
Fighter-kun <carlosgarciacachon@gmail.com>
2024-05-08 13:00:49 +0200

    MODIFIED - Eliminar lineas pedidos

--- src/main/java/curso/java/tienda/controller/PedidoController.java
+++ src/main/java/curso/java/tienda/controller/PedidoController.java
@@ -1,5 +1,7 @@
 package curso.java.tienda.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,14 +11,20 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import curso.java.tienda.model.CategoriaVO;
+import curso.java.tienda.model.DetallePedidoVO;
 import curso.java.tienda.model.PedidoVO;
 import curso.java.tienda.repository.PedidoRepository;
+import curso.java.tienda.repository.DetallePedidoRepository;
 
 @Controller
 public class PedidoController {
 
 	@Autowired
 	private PedidoRepository pedidoRepository;
+	
+	@Autowired
+	private DetallePedidoRepository detallePedidoRepository;
+
 
 	@GetMapping("/pedidos")
 	public String findAll(Model model) {
@@ -30,9 +38,23 @@ public class PedidoController {
     public String cambiarEstadoPedido(@PathVariable Integer id) {
         pedidoRepository.findById(id).ifPresent(existingPedido -> {
             existingPedido.setEstado("E");
+
+            // Recuperar todas las líneas de pedido para este pedido específico
+            List<DetallePedidoVO> detallesPedido = detallePedidoRepository.findByIdPedido(id);
+
+            // Calcular el total sumando el valor total de cada línea de pedido
+            double totalPedido = detallesPedido.stream()
+                    .mapToDouble(detallePedido -> detallePedido.getTotal())
+                    .sum();
+
+            // Asignar el total calculado al pedido existente
+            existingPedido.setTotal(totalPedido);
+            
+            // Guardar los cambios en el pedido
             pedidoRepository.save(existingPedido);
         });
         return "redirect:/pedidos";
     }
+
 
 }
